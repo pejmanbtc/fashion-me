6200ff1dd132df60d1ef92f5dbe4e7ad17a387f5
pejmanbtc <ebrahimip109@gmail.com>
2023-02-14 00:42:31 +0330

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const btn1menu=document.querySelector('.btn1');
+const mobilmenu=document.querySelector('.mbilmenu');
+btn1menu.addEventListener('click',()=>{
+    btn1menu.classList.toggle('btn2mbil');
+    mobilmenu.classList.toggle('mbildisnon');
+});
+
+// serch tblet
+// const inpsertab=document.querySelector('.SerLT');
+// const imgsertb=document.getElementById('imgsertb');
+// imgsertb.addEventListener('click',()=>{
+//     inpsertab.classList.toggle('sertogg');
+// })
