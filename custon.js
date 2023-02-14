049e407f0ca48ebdd56fe6b278a3635b80fd57ef
pejmanbtc <ebrahimip109@gmail.com>
2023-02-14 14:06:17 +0330

    slider

--- custon.js
+++ custon.js
@@ -5,19 +5,19 @@ const swiper = new Swiper(".swiper", {
 
   // If we need pagination
   pagination: {
-    el: ".swiper-pagination"
+    el: ".swiper-pagination",
   },
 
   // Navigation arrows
   navigation: {
     nextEl: ".next",
-    prevEl: ".prev"
+    prevEl: ".prev",
   },
 
   breakpoints: {
     300: { slidesPerView: 1 },
     768: { slidesPerView: 2 },
     1200: { slidesPerView: 4 },
-    1400: { slidesPerView: 4 }
-  }  
+    1400: { slidesPerView: 4 },
+  },
 });
